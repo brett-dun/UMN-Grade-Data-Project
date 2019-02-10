07c2fa529067f45ac06b1b6fac58bec60c8acc2d
Brett Duncan <brettd314@gmail.com>
2019-02-09 21:11:17 -0600

    first commit

---
+++ grade_data.js
@@ -0,0 +1,88 @@
+var DATA = {
+	"CSCI": {
+		"1001": ["Overview of Computer Science", 3., 50],
+		"1113": ["Introduction to C/C++ Programming for Scientists and Engineers", 3., 50],
+		"1133": ["Introduction to Computing and Programming Concepts", 2.1, 200],
+		"1913": ["Introduction to Algorithms, Data Structures, and Program Development", 3., 50],
+		"1933": ["Introduction to Algorithms and Data Structures", 3.75, 200],
+		"1933H": ["Honors Introduction to Algorithms and Data Structures", 3., 50],
+
+		"2011": ["Discrete Structures of Computer Science", 3.5, 150],
+		"2011H": ["Honors Discrete Structures of Computer Science", 3., 50],
+		"2021": ["Machine Architecture and Organization", 3.25, 100],
+		"2033": ["Elementary Computational Linear Algebra", 3., 100],
+		"2041": ["Advanced Programming Principles", 2.75, 100],
+
+		"3003": ["Introduction to Computing in Biology", 3., 50],
+		"3081W": ["Program Design and Development", 3., 50],
+		"3970": ["Industrial Student Co-op Assignment", 3., 50],
+
+		"4011": ["Formal Languages and Automata Theory", 3., 50],
+		"4041": ["Algorithms and Data Structures", 2.5, 100],
+		"4061": ["Introduction to Operating Systems", 2.25, 100],
+		"4131": ["Internet Programming", 3., 50],
+		"4203": ["Computer Architecture", 3., 50],
+		"4211": ["Introduction to Computer Networks", 3., 50],
+		"4511W": ["Introduction to Artificial Intelligence", 3., 50],
+		"4611": ["Programming Interactive Computer Graphics and Games", 3., 50],
+		"4707": ["Practice of Database Systems", 3., 50],
+		"4950": ["Senior Software Project", 3., 50],
+		"4970W": ["Advanced Project Laboratory", 3., 50],
+		"4994H": ["Honors Thesis", 3., 50],
+
+		"5103": ["Operating Systems", 3., 50],
+		"5105": ["Introduction to Distributed Systems", 3., 50],
+		"5115": ["User Interface Design, Implementation and Evaluation", 3., 50],
+		"5125": ["Collaborative and Social Computing", 3., 50],
+		"5143": ["Real-Time and Embedded Systems", 3., 50],
+		"5161": ["Introduction to Compilers", 3., 50],
+		"5271": ["Introduction to Computer Security", 3., 50],
+		"5302": ["Analysis of Numerical Algorithms", 3., 50],
+		"5421": ["Advanced Algorithms and Data Structures", 3., 50],
+		"5451": ["Introduction to Parallel Computing: Architectures, Algorithms, and Programming", 3., 50],
+		"5461": ["Functional Genomics, Systems Biology, and Bioinformatics", 3., 50],
+		"5465": ["Introduction to Computing for Biologists", 3., 50],
+		"5471": ["Modern Cryptography", 3., 50],
+		"5512": ["Artificial Intelligence II", 3., 50],
+		"5521": ["Introduction to Machine Learning", 3., 50],
+		"5523": ["Introduction to Data Mining", 3., 50],
+		"5561": ["Computer Vision", 3., 50],
+		"5609": ["Visualization", 3., 50],
+		"5611": ["Animation & Planning in Games", 3., 50],
+		"5708": ["Architecture and Implementation of Database Management Systems", 3., 50],
+		"5801": ["Software Engineering I", 3., 50],
+		"5802": ["Software Engineering II", 3., 50],
+		"5991": ["Independent Study", 3., 50],
+		"5994": ["Directed Research", 3., 50],
+		"5996": ["Curricular Practical Training"],
+	},
+	"EE": {
+		"1301": ["Introduction to Computing Systems", 2.7, 100],
+		"2001": ["Introduction to Circuits and Electronics", 3.1, 50],
+		"2002": ["Introductory Circuits and Electronics Laboratory", 2.2, 50],
+	},
+	"ME": {
+
+	},
+	"CHEN": {
+
+	},
+	"BMEN": {
+
+	},
+	"AEM": {
+
+	},
+	"MATH": {
+
+	},
+	"STAT": {
+
+	},
+	"PHYS": {
+
+	},
+	"CHEM": {
+
+	},
+}
